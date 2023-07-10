a98d57847b4401b62b7fd47fffd78ff6511f13e6
sheready <csstlw@gmail.com>
2023-07-10 23:18:42 +0300

    add  about us component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import img1 from '../media/events/intent.jpeg'
+import img2 from '../media/events/money.jpeg'
+import img3 from '../media/events/opp.jpeg'
+import img4 from '../media/events/single.jpeg'
+import img5 from '../media/events/social.jpeg'
+import img6 from '../media/events/tech.jpeg'
 
 const About = () => {
   return (
     <>
-    <h5 class="pt-4 font-bold text-center font-myfrida text-light-blue text-xl">
+    <h5 class="pt-4 font-bold text-center font-myfrida text-light-blue text-2xl">
         WHY SHOULD YOU JOIN US?
     </h5>
     <div class="container flex flex-col justify-center px-4 mx-auto mt-5 space-y-8 lg:space-y-0 lg:flex-row">     
@@ -28,9 +34,29 @@ const About = () => {
             <p class="mb-1 font-normal text-center text-myriad text-dark-blue ">After every event women should leave feeling heard, with hope, equipped with immediate action plans and where possible a set of new friends.</p>
         </div>
     </div>
-
-    
-    
+    <h5 class="pt-4 font-bold text-center font-myfrida text-light-blue text-2xl">
+        WHICH ARE SOME OF OUR PAST EVENTS?
+    </h5>
+    <div class="container flex flex-col justify-center px-4 mx-auto mt-5 space-y-8 lg:space-y-0 lg:flex-row">     
+        <div class="flex flex-col rounded-lg lg:mr-2">
+            <img src={img1} alt="event-1" />
+        </div>
+        <div class="flex flex-col rounded-lg lg:mr-2">
+            <img src={img2} alt="event-2" />
+        </div>
+        <div class="flex flex-col rounded-lg lg:mr-2">
+            <img src={img3} alt="event-3" />
+        </div>
+        <div class="flex flex-col rounded-lg lg:mr-2">
+            <img src={img4} alt="event-4" />
+        </div>
+        <div class="flex flex-col rounded-lg lg:mr-2">
+            <img src={img5} alt="event-5" />
+        </div>
+        <div class="flex flex-col rounded-lg lg:mr-2">
+            <img src={img6} alt="event-6" />
+        </div>
+    </div> 
     </>
   )
 }
