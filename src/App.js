11f779b903a9b716e8f0c615733d51f2c3622158
sheready <csstlw@gmail.com>
2024-01-25 23:56:15 +0300

    add kampala route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Register from './components/Register';
 import Member from './components/Member';
 import Profile from './components/Profile';
 import Meru from './components/Meru';
+import Machakos from './components/Machakos';
+import Kiambu from './components/Kiambu';
+import Uganda from './components/Uganda';
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/meru" element={<Meru />} />
+        <Route path="/machakos" element={<Machakos />} />
+        <Route path="/kiambu" element={<Kiambu />} />
+        <Route path="/kampala" element={<Uganda/>} />
         <Route path="/circles-of-trust" element={<Circles/>}  />
         <Route path="/join-the-movement" element={<Sponsor/>} />
         <Route path="/partner" element={<Partner/>} />
