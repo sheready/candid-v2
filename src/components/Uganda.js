7a8d6318efa5a7df33780bd49376da18e3a7a668
sheready <csstlw@gmail.com>
2024-02-08 20:36:16 +0300

    edit page layout and font

--- src/components/Uganda.js
+++ src/components/Uganda.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import pot1 from '../media/kampala/53147016967_3fb1f6bf80_c.jpg'
-import pot2 from '../media/kampala/53147590741_320070f0ee_k.jpg'
+import pot1 from '../media/kampala/53147798189_2a0047d390_c.jpg'
+import pot2 from '../media/kampala/53148010670_3f64234747_c.jpg'
 import pot3 from '../media/kampala/53147571536_07158e6ef3_c.jpg'
 import pot4 from '../media/kampala/53147590741_320070f0ee_k.jpg'
 import land1 from '../media/kampala/53147016967_3fb1f6bf80_c.jpg'
@@ -15,21 +15,24 @@ import land9 from '../media/kampala/53147786049_baa961de88_c.jpg'
 const Uganda = () => {
   return (
     <div>
-      <section class="mt-[12%] md:mt-[7%] bg-center bg-no-repeat bg-[url('https://candid-backend.s3.amazonaws.com/banner/53147000502_a90ce9918d_c.jpg')] bg-gray-400 bg-blend-multiply bg-cover h-[50vh] lg:h-[70vh]">
-        <div class="px-4 mx-auto max-w-screen-xl text-center py-44 md:py-72 lg:py-56 xl:py-68">
-            <h1 class="mb-4 text-4xl font-myfrida tracking-tight leading-none text-indigo md:text-6xl lg:text-6xl">DEFINING FINANCIAL SUCCESS</h1>
-        </div>
-      </section>
-      <h5 class="pt-4 font-bold text-center font-myfrida text-dark-blue text-2xl">
-        HIGHLIGHTS
-      </h5>
+       <section >
+            <section class="bg-center bg-no-repeat bg-[url('/home/stella/Documents/projects/Candid/candid-v2/candid-v2/src/media/kampala/53148004435_f7bb34dc0e_c.jpg')] bg-gray-400 bg-blend-multiply bg-cover">
+                <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-indigo font-myfrida md:text-5xl lg:text-7xl">Candid kampala 2023</h1>
+                    
+                </div>
+            </section>       
+        </section> 
+        <h5 class="pt-4 font-bold text-center font-myfrida text-dark-blue text-3xl md:text-5xl mt-6 md:my-6">
+            HIGHLIGHTS
+        </h5>
       <div class="grid mt-6 m-4 grid-cols-2 md:grid-cols-4 gap-4">
           <div class="grid gap-4">
           <div>
-              <img class="h-auto max-w-full rounded-lg" src={pot1} alt=""/>
+              <img class="h-auto max-w-full rounded-lg" src={land1} alt=""/>
           </div>
           <div>
-              <img class="h-auto max-w-full rounded-lg" src={land1} alt=""></img>
+              <img class="h-auto max-w-full rounded-lg" src={pot1} alt=""></img>
           </div>
           <div>
               <img class="h-auto max-w-full rounded-lg" src={land9} alt=""></img>
