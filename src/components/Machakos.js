a4393e27672b58061dd5f51053aa5b00ce6e18c8
sheready <csstlw@gmail.com>
2024-02-08 20:30:05 +0300

    edit machakos banner and font

--- src/components/Machakos.js
+++ src/components/Machakos.js
@@ -22,36 +22,46 @@ import land9 from '../media/machakos/Candid Conversation IMG_9970.jpg'
 const Meru = () => {
   return (
     <div>
-        <section class="mt-[12%] md:mt-[7%] bg-center bg-no-repeat bg-[url('https://candid-backend.s3.amazonaws.com/banner/Candid+Conversations+IMG_4066.jpg')] bg-gray-400 bg-blend-multiply bg-cover h-[50vh] lg:h-[70vh]">
-            <div class="px-4 mx-auto max-w-screen-xl text-center py-44 md:py-72 lg:py-56 xl:py-68">
-              <h1 class="mb-4 text-4xl font-myfrida tracking-tight leading-none text-indigo md:text-6xl lg:text-6xl">DEFINING FINANCIAL SUCCESS</h1>
-            </div>
+        <section>
+            <section class="bg-center bg-no-repeat bg-[url('/home/stella/Documents/projects/Candid/candid-v2/candid-v2/src/media/machakos/img.jpg')] bg-gray-400 bg-blend-multiply bg-cover">
+                <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-indigo font-myfrida md:text-5xl lg:text-7xl">Candid Machakos 2023</h1>
+                    
+                </div>
+            </section>       
         </section>
-        <h4 class="font-myfrida text-dark-blue font-bold text-2xl text-center">About the Event</h4>
-        <div class="container flex flex-col px-12 mx-auto mt-6 space-y-6  mb-4 md:space-x-2 md:space-y-0 md:flex-row">
-            <div class="flex flex-col space-y-8 md:space-y-5 md:mt-10  md:w-3/4">
-                <p class="max-w-sm text-left font-myriad ">
-                    This debut cocktail event at Machakos will focus on providing a platform for women from Machakos to share their unconventional paths to success through an evening of moderated conversation and intimate breakout sessions.
-                </p>
-                <p class="max-w-sm text-left font-myriad">
-                    We’ll be learning from our panelists’ personal stories as we explore both business and career  opportunities that could accelerate your current growth. This will be done through introspection and group reflection in the breakout sessions.
-                </p>
-            </div>
-            <div class="flex flex-col space-y-4 md:mt-10 md:w-3/4">
-                <div class="flex flex-col justify-center space-y-4 md:mt-8">
+        <h4 class="pt-4 font-bold text-center font-myfrida text-dark-blue text-3xl md:text-5xl mt-6 md:my-6">ABOUT THE EVENT</h4>
+        <div className='grid grid-cols-1 mx-10 md:grid-cols-2'>
+            <div className=''>     
+                <div class="flex flex-col justify-center space-y-4">
                     <h6 class="font-myfrida text-dark-blue font-bold text-2xl">Session objective</h6>
-                    <p class="max-w-sm text-left font-myriad">
+                    <p class="text-left font-myriad text-xl">
                         To maximize community cohesion, promote networking, and celebrate the local business landscape in Machakos.
                     </p>
                 </div>
-                <div class="flex justify-center md:justify-start">
+
+                <div class="flex justify-center mt-6 md:justify-start">
                     <a href='https://bit.ly/CandidMachakos' class="p-2 px-6 pt-2 font-myfrida  bg-light-blue rounded-full baseline hover:bg-light-blue">
                         REGISTER NOW
                     </a>
                 </div>
             </div>
+            <div className='mx-10 space-y-4'>
+                <p class="text-left font-myriad text-xl">
+                    This debut cocktail event at Machakos will focus on providing a platform for women from Machakos to share their unconventional paths to success through an evening of moderated conversation and intimate breakout sessions.
+                </p>
+                <p class="text-left font-myriad text-xl">
+                    We’ll be learning from our panelists’ personal stories as we explore both business and career  opportunities that could accelerate your current growth. This will be done through introspection and group reflection in the breakout sessions.
+                </p>
+            </div>
         </div>
-        <div class="">
+        <div class="container flex flex-col px-12 mx-auto mt-6 space-y-6  mb-4 md:space-x-2 md:space-y-0 md:flex-row">
+            <div class="flex flex-col space-y-8 md:space-y-5 md:mt-10  md:w-3/4">
+                
+            </div>
+            
+        </div>
+        {/* <div class="">
             <div class="flex justify-center pt-5">
                 <h2 class="text-dark-blue font-bold font-myfrida text-2xl">EVENT SPEAKERS</h2>
             </div>
@@ -73,11 +83,11 @@ const Meru = () => {
                     <img class="h-auto max-w-full rounded-lg" src={bio4} alt=""/>
                 </div>
             </div>
-        </div>
-        <h5 class="pt-4 font-bold text-center font-myfrida text-dark-blue text-2xl">
-            IMAGES FROM CANDID CONVERSATION MACHAKOS 2023
+        </div> */}
+        <h5 class="pt-4 font-bold text-center font-myfrida text-dark-blue text-3xl md:text-5xl mt-6 md:my-6">
+            HIGHLIGHTS
         </h5>
-        <div class="grid mt-6 mx-4 grid-cols-2 md:grid-cols-4 gap-4">
+        <div class="grid mt-6 mx-4 grid-cols-2 md:grid-cols-4 gap-4 mb-4">
             <div class="grid gap-4">
             <div>
                 <img class="h-auto max-w-full rounded-lg" src={pot1} alt=""/>
